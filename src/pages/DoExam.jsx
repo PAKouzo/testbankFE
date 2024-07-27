cb981171f091c865989032a2c10d778a5ab05a5f
nguyenbathao <thaofamily8900nh@gmail.com>
2024-07-27 17:05:59 +0700

    exam fe

--- src/pages/DoExam.jsx
+++ src/pages/DoExam.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Layout from '../components/Layout/layout';
 import axios from 'axios';
-import { Checkbox } from 'antd';
 import '../styles/DoExam.css';
 
 const DoExam = () => {
@@ -11,8 +10,7 @@ const DoExam = () => {
   const [exam, setExam] = useState({});
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState({});
-  const [remainingTime, setRemainingTime] = useState(0);
-  const timerRef = useRef(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
   const getExam = async () => {
     try {
@@ -28,83 +26,129 @@ const DoExam = () => {
     getExam();
   }, [_id]);
 
-  useEffect(() => {
-    if (remainingTime > 0) {
-      timerRef.current = setInterval(() => {
-        setRemainingTime((prevTime) => {
-          if (prevTime <= 1) {
-            clearInterval(timerRef.current);
-            handleSubmitExam();
-            return 0;
-          }
-          return prevTime - 1;
-        });
-      }, 1000);
-    }
-    return () => clearInterval(timerRef.current);
-  }, [remainingTime]);
-
-  const formatTime = (seconds) => {
-    const minutes = Math.floor(seconds / 60);
-    const secs = seconds % 60;
-    return `${minutes}:${secs < 10 ? `0${secs}` : secs}`;
+  const handleSingleAnswerChange = (questionId, answer) => {
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [questionId]: answer,
+    }));
   };
 
-  const handleAnswerChange = (questionId, answer) => {};
+  const handleMultipleAnswerChange = (questionId, answer) => {
+    setAnswers((prevAnswers) => {
+      const currentAnswers = prevAnswers[questionId] || [];
+      if (currentAnswers.includes(answer)) {
+        return {
+          ...prevAnswers,
+          [questionId]: currentAnswers.filter((a) => a !== answer),
+        };
+      } else {
+        return {
+          ...prevAnswers,
+          [questionId]: [...currentAnswers, answer],
+        };
+      }
+    });
+  };
+
+  const handleSubmitExam = async () => {
+    try {
+      const response = await axios.post(`http://localhost:8080/api/exam/submit-exam/${_id}`, {
+        answers,
+      });
+      if (response.data?.success) {
+        navigate('/dashboard/student/results');
+      } else {
+        console.log(response.data?.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  };
+
+  const handlePreviousQuestion = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+  };
+
+  const currentQuestion = questions[currentQuestionIndex];
 
   return (
     <Layout>
       <div className="col-md-8 exam-take">
         <h1 className="text-center">Take Exam: {exam?.name}</h1>
-        <div className="timer">
-          <h4>Time Remaining: {formatTime(remainingTime)}</h4>
-        </div>
-        <hr />
-        {questions.map((question, index) => (
-          <div key={question._id} className="question-block">
+        {currentQuestion && (
+          <div key={currentQuestion._id} className="question-block">
             <h5>
-              Question {index + 1}: {question.content}
+              Question {currentQuestionIndex + 1}: {currentQuestion.content}
             </h5>
-            {question.type === 'Text-Input' ? (
+            {currentQuestion.type === 'Text-Input' ? (
               <div className="text-input">
                 <input
                   type="text"
-                  onChange={(e) => handleAnswerChange(question._id, e.target.value)}
+                  onChange={(e) => handleSingleAnswerChange(currentQuestion._id, e.target.value)}
                   placeholder="Type your answer here"
                 />
               </div>
+            ) : currentQuestion.type === 'Multiple-Choice' ? (
+              <div className="choice">
+                {['answer', 'answer1', 'answer2', 'answer3', 'answer4'].map((key) => (
+                  <div key={key}>
+                    <input
+                      type="checkbox"
+                      name={`question-${currentQuestion._id}`}
+                      value={currentQuestion[key]}
+                      onChange={(e) =>
+                        handleMultipleAnswerChange(currentQuestion._id, e.target.value)
+                      }
+                    />
+                    {currentQuestion[key]}
+                  </div>
+                ))}
+              </div>
             ) : (
               <div className="choice">
-                <Checkbox
-                  onChange={(e) => handleAnswerChange(question._id, e.target.value)}
-                  value={question.answer1}
-                >
-                  {question.answer1}
-                </Checkbox>
-                <Checkbox
-                  onChange={(e) => handleAnswerChange(question._id, e.target.value)}
-                  value={question.answer2}
-                >
-                  {question.answer2}
-                </Checkbox>
-                <Checkbox
-                  onChange={(e) => handleAnswerChange(question._id, e.target.value)}
-                  value={question.answer3}
-                >
-                  {question.answer3}
-                </Checkbox>
-                <Checkbox
-                  onChange={(e) => handleAnswerChange(question._id, e.target.value)}
-                  value={question.answer4}
-                >
-                  {question.answer4}
-                </Checkbox>
+                {['answer1', 'answer2', 'answer3', 'answer4'].map((key) => (
+                  <div key={key}>
+                    <input
+                      type="radio"
+                      name={`question-${currentQuestion._id}`}
+                      value={currentQuestion[key]}
+                      onChange={(e) =>
+                        handleSingleAnswerChange(currentQuestion._id, e.target.value)
+                      }
+                    />
+                    {currentQuestion[key]}
+                  </div>
+                ))}
               </div>
             )}
           </div>
-        ))}
-
-        <button className="btn btn-primary">Submit Exam</button>
+        )}
+        <div className="navigation-buttons">
+          <button
+            className="btn btn-secondary"
+            onClick={handlePreviousQuestion}
+            disabled={currentQuestionIndex === 0}
+          >
+            Previous
+          </button>
+          {currentQuestionIndex < questions.length - 1 ? (
+            <button className="btn btn-secondary" onClick={handleNextQuestion}>
+              Next
+            </button>
+          ) : (
+            <button className="btn btn-primary" onClick={handleSubmitExam}>
+              Submit Exam
+            </button>
+          )}
+        </div>
       </div>
     </Layout>
   );
